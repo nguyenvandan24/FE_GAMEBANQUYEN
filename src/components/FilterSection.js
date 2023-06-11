d960fe492fb5782d05e719ec1713545122ddb387
NguyenThiHau <87900120+haulo@users.noreply.github.com>
2023-06-11 20:04:38 +0700

    update product page

--- src/components/FilterSection.js
+++ src/components/FilterSection.js
@@ -1,7 +1,67 @@
 import React from "react";
+import styled from "styled-components";
+import {useFilterContext} from "../context/filter_context";
 
 const FilterSection = () => {
-    return <div>FilterSection</div>
-};
+    const {
+        filters: { text, category}, updateFilterValue, all_products
+    } = useFilterContext();
+    //to get the unique data of earch  fields
+    const getUniqueData = (data, attr) =>{
+        let  newVal = data.map((curElem) =>{
+            return curElem[attr];
 
-export default FilterSection;
+        });
+
+        return (newVal = ["All",...new Set(newVal)]);
+        //console.log(newVal);
+    }
+    //we need to have the individual data of each in an array format
+    const categoryOnlyData= getUniqueData(all_products, "category");
+
+    return <Wrapper>
+        <div className="filter-search">
+            <form onSubmit={(e) => e.preventDefault()}>
+                <input type="text" name="text" value={text} onChange={updateFilterValue} placeholder="Search" />
+            </form>
+        </div>
+        <div className="filter-category">
+            <h3> Category</h3>
+            <div>
+                {categoryOnlyData.map((curElem, index) => {
+                    return <button key = {index}
+                        type="button" name =" category" value={category} onClick={updateFilterValue}>
+                        {curElem}
+                    </button>
+
+                })}
+            </div>
+        </div>
+    </Wrapper>
+};
+const Wrapper = styled.section`
+  padding: 5rem 0;
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+  h3 {
+    padding: 2rem 0;
+    font-size:medium;
+  }
+  .filter-search {
+    input {
+      padding: 0.6rem 1rem;
+      width: 80%;
+    }
+  }
+  .filter-category{
+      div{
+        display: flex;
+        flex-direction: column;
+        
+      }
+  }
+
+`;
+
+    export default FilterSection;
--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,7 +1,89 @@
 import React from "react";
+import styled from "styled-components";
+import {BsFillGridFill, BsList} from "react-icons/bs";
+import {useFilterContext} from "../context/filter_context";
 
 const Sort = () => {
-    return <div>Sort</div>
-};
+    const { filter_products, grid_view, setGridView, setListView, sorting} = useFilterContext();
 
+    return (
+        <Wrapper className="sort-section">
+            {/* 1st column */}
+            <div className="sorting-list--grid">
+                <button className= {grid_view ? "active sort-btn" : "sort-btn"}
+                    onClick={setGridView}>
+                    <BsFillGridFill className="icon" />
+                </button>
+                <button className= { !grid_view ? "active sort-btn" : " sort-btn"}
+                    onClick={setListView}>
+                    <BsList className="icon"/>
+                </button>
+            </div>
+            {/* 2st column */}
+            <div className="product-data">
+                {/* tong san pham*/}
+                <p>{`${filter_products.length} Product Available`}</p>
+            </div>
+            {/* 3rd column */}
+            <div className="sort-section">
+                <form action="#">
+                    <label htmlFor="sort"></label>
+                    <select form="sort" id="sort" className="sort-seclect--style" onClick={sorting}>
+                        <option value="lowest">Price(lowest)</option>
+                        <option value="#" disabled></option>
+                        <option value="lowest">Price(highest)</option>
+                        <option value="#" disabled></option>
+                        <option value="lowest">Price(a-z)</option>
+                        <option value="#" disabled></option>
+                        <option value="lowest">Price(z-a)</option>
+
+                    </select>
+                </form>
+            </div>
+
+
+
+
+        </Wrapper>
+    )
+};
+const Wrapper = styled.section`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 5rem;
+
+  .sorting-list--grid {
+    display: flex;
+    gap: 2rem;
+
+    .sort-btn {
+      padding: 0.8rem 0.8rem;
+      border: none;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      cursor: pointer;
+    }
+
+    .icon {
+      font-size: 1.6rem;
+    }
+    .active {
+      background-color: ${({ theme }) => theme.colors.black};
+      color: #61dafb;
+    }
+  }
+
+  .sort-selection .sort-selection--style {
+    padding: 0.5rem;
+    cursor: pointer;
+
+    .sort-select--option {
+      padding: 0.5rem 0;
+      cursor: pointer;
+      height: 2rem;
+      padding: 10px;
+    }
+  }
+`;
 export default Sort;
